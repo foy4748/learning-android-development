a6bb8d1d97023c93fb6d9bdae15031059e1ec863
foy4748 <faisaljfcl@gmail.com>
2025-07-03 00:56:39 +0600

    Article Text Section - DONE

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -9,18 +9,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.greetingcard.article.ArticleText
 import com.example.greetingcard.ui.theme.GreetingCardTheme
 import com.example.greetingcard.ui.theme.HappyBirthDayTheme
 
@@ -42,8 +50,8 @@ class MainActivity : ComponentActivity() {
                     Surface(
                         color = MaterialTheme.colorScheme.background,
                         modifier = Modifier
-//                            .background(color = Color.Red)
-                            .padding(innerPadding),
+                            .background(color = Color.Red)
+                            .padding(innerPadding)
                     ) {
                         BirthdayCardPreview()
 
@@ -88,5 +96,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
-    BirthdayCardPicture("Faisal", "May all your wishes come true")
+//    BirthdayCardPicture("Faisal", "May all your wishes come true")
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        ArticleText(modifier = Modifier.padding(14.dp))
+    }
 }
---
+++ app/src/main/java/com/example/greetingcard/article/ArticleTextSection.kt
@@ -0,0 +1,34 @@
+package com.example.greetingcard.article
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ArticleText(modifier: Modifier = Modifier) {
+
+    Column(verticalArrangement = Arrangement.spacedBy(12.dp), modifier = modifier) {
+        // Title
+        Text(
+            text = "Jetpack Compose tutorial",
+            fontSize = 24.sp
+        )
+        // Paragraph 1
+        Text(
+            text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+            fontSize = 12.sp,
+            textAlign = TextAlign.Justify
+        )
+        // Paragraph 2
+        Text(
+            text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+            fontSize = 12.sp,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
