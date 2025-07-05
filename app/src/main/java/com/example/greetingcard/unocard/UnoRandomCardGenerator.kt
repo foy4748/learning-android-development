abd6ea4c842e14710e1e6675cb268750b9097c4e
foy4748 <faisaljfcl@gmail.com>
2025-07-05 18:26:52 +0600

    Added Oval in the Card. Tried to make it look like UNO cards

--- app/src/main/java/com/example/greetingcard/unocard/UnoRandomCardGenerator.kt
+++ app/src/main/java/com/example/greetingcard/unocard/UnoRandomCardGenerator.kt
@@ -3,7 +3,7 @@ package com.example.greetingcard.unocard
 object UnoCard{
     val colors  = arrayOf(0xFF000000, 0xFFE74C3C, 0xFF2ECC71, 0xFFF1C40F, 0xFF3498DB)
     val numbers = arrayOf("0","1","2","3","4","5","6","7","8","9","+2")
-    val wild = arrayOf("+4", " ")
+    val wild = arrayOf("+4", "Wild")
 
     fun getRandomCard(): Pair<Long, String> {
         val aRandomColor = colors.random()
--- app/src/main/java/com/example/greetingcard/unocard/UnoRandomCardRender.kt
+++ app/src/main/java/com/example/greetingcard/unocard/UnoRandomCardRender.kt
@@ -1,7 +1,9 @@
 package com.example.greetingcard.unocard
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,9 +12,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -20,40 +27,97 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun UnoRandomCardRender() {
     val (cardColor, cardSymbol) = UnoCard.getRandomCard()
-    val padding = 30.dp
-    Column {
-        Row(
-            horizontalArrangement = Arrangement.Start,
-            verticalAlignment = Alignment.Top,
-            modifier = Modifier
-            .background(Color(cardColor))
-                .fillMaxSize()
-                .weight(1f)
-        ) {
-            Text(
-                text = cardSymbol,
-                color = Color(0xFFFFFFFF),
-                fontSize = 50.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(padding)
-            )
+    val padding : Dp = 30.dp
+    val fontSize : TextUnit = 100.sp
+
+    // Card Oval
+    Box {
+        Column {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.Top,
+                modifier = Modifier
+                    .background(Color(cardColor))
+                    .fillMaxSize()
+                    .weight(1f)
+            ) {
+                Text(
+                    text = cardSymbol,
+                    color = Color(0xFFFFFFFF),
+                    fontSize = fontSize,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(padding)
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.Bottom,
+                modifier = Modifier
+                    .background(Color(cardColor))
+                    .fillMaxSize()
+                    .weight(1f)
+            ) {
+                Text(
+                    text = cardSymbol,
+                    color = Color(0xFFFFFFFF),
+                    fontSize = fontSize,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(padding)
+                        .rotate(180f)
+                )
+            }
+
         }
-        Row (
-            horizontalArrangement = Arrangement.End,
-            verticalAlignment = Alignment.Bottom,
-            modifier = Modifier
-                .background(Color(cardColor))
-                .fillMaxSize()
-                .weight(1f)
-        ) {
-            Text(
-                text = cardSymbol,
-                color = Color(0xFFFFFFFF),
-                fontSize = 50.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(padding)
-            )
+
+        Box{
+            // Oval
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Transparent)
+            ) {
+                Canvas(
+                    modifier = Modifier
+//                    .size(200.dp)
+                        .fillMaxSize()
+                        .rotate(45f) // Rotate the oval diagonally
+                ) {
+                    drawOval(
+                        color = Color.White, // Change to your desired color
+                        topLeft = Offset.Zero,
+                        size = size,
+                        style = Stroke(width = 40.dp.toPx()) // Outline thickness
+                    )
+                }
+            }
+            // END of Oval
+
+            // Mid Text
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Transparent)
+
+            ){
+                val largeFont : TextUnit = 200.sp
+                Text(
+                    text = cardSymbol,
+                    color = Color(0xFFFFFFFF),
+                    fontSize = largeFont,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(padding)
+                )
+
+            }
+            // END Mid Text
         }
 
     }
+
+    // Card Base
 }
