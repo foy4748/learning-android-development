c74cdd305338e290eb1fc2b6831e79d283999a5b
foy4748 <faisaljfcl@gmail.com>
2025-07-16 01:56:05 +0600

    Changed Default Icon
    The colors.xml, strings.xml, themes.xml files are not provided by
    
    https://easyappicon.com/
    
    in the values folder. make sure you use it from default / prefious
    project or so.

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -7,15 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.MaterialTheme
@@ -23,24 +20,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.example.greetingcard.affirmation.AffirmationRender
-import com.example.greetingcard.artgallery.ArtGalleryRender
-import com.example.greetingcard.article.ArticlePicture
-import com.example.greetingcard.article.ArticleTask
-import com.example.greetingcard.article.ArticleText
-import com.example.greetingcard.birthdaycard.BirthdayCardRender
-import com.example.greetingcard.businesscard.BusinessCardRender
-import com.example.greetingcard.quadrant.Quadrant
-import com.example.greetingcard.task.TaskCheckerTask
-import com.example.greetingcard.task.TaskPicture
-import com.example.greetingcard.task.TaskText
 import com.example.greetingcard.ui.theme.GreetingCardTheme
 import com.example.greetingcard.ui.theme.HappyBirthDayTheme
 
@@ -119,8 +104,8 @@ fun BirthdayCardPreview() {
 //    ArticleTask()
 //    Quadrant()
 //    BusinessCardRender()
-//    AffirmationRender()
-    ArtGalleryRender()
+    AffirmationRender()
+//    ArtGalleryRender()
 }
 
 
