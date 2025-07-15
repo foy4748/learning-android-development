80c880936c35965ad85b40e4ef0dec95de6307ce
foy4748 <faisaljfcl@gmail.com>
2025-07-15 12:01:02 +0600

    Added @DrawableRes in type of resourceId of GalleryPhoto class

--- app/src/main/java/com/example/greetingcard/artgallery/GalleryPhoto.kt
+++ app/src/main/java/com/example/greetingcard/artgallery/GalleryPhoto.kt
@@ -1,10 +1,11 @@
 package com.example.greetingcard.artgallery
 
+import androidx.annotation.DrawableRes
 import java.time.LocalDate
 import java.util.Date
 
 data class GalleryPhoto(
-    val resourceId: Int,
+    @DrawableRes val resourceId: Int,
     val licenseType: String,
     val publishDate: Date,
     val location: String? = null
